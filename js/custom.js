5e17ca72b38f00426aee321ab718c45954f0d626
Chandra Prakash J Patel <90178452+bittujpatel@users.noreply.github.com>
2023-05-10 18:52:54 +0000

    Updated Timeline and Age

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,19 @@ function getYear() {
 }
 
 getYear();
+
+// to get age year
+
+function calculateAge(birthday) {
+    var ageDifMs = Date.now() - new Date(birthday).getTime();
+    var ageDate = new Date(ageDifMs);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
+function showAge() {
+    var Bdate = "2000-10-06"; // your date of birth in yyyy-mm-dd format
+    var age = calculateAge(Bdate);
+    document.getElementById('age').innerHTML = age;
+}
+
+window.onload = showAge; // call the showAge function when the page loads
